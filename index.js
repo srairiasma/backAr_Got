878b0e6e6a32b0785c72146e08d97b72ead9094c
srairiasma <67099232+srairiasma@users.noreply.github.com>
2023-09-01 19:45:00 +0100

    ezez

--- index.js
+++ index.js
@@ -1,45 +1,49 @@
-import express from "express"
-import session from 'express-session'
-import dotenv from "dotenv"
-import bodyParser from "body-parser"
-import mongoose from "mongoose"
-import auth from "./routes/AuthentificationRoute.js" 
-import shopRoutes from './routes/shop.js';
-import User from "./model/User.js"
+import express from "express";
+import session from "express-session";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors"; // Add CORS support
 
-//import shop from "./routes/ShopItem.js"
-const app = express()
+import auth from "./routes/AuthentificationRoute.js";
+import shopRoutes from "./routes/shop.js";
 
-app.use(bodyParser.urlencoded({extended : false}))
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
 
+mongoose.set("strictQuery", true);
 
-mongoose.set('strictQuery', true);
-//setting the database
-dotenv.config() ; 
-export const connect = async () => {
-    try {
-    await mongoose.connect("mongodb+srv://asmasrairi:56isr0D9O7p34eY0@cluster0.2bdnucf.mongodb.net/Ar_Got");
-    
-    } catch (error) {
-        throw error
-    }
-}
- 
 // Set up session
-app.use(session({
-    secret: 'asma',
+app.use(
+  session({
+    secret: "asma",
     resave: false,
-    saveUninitialized: true
-  }));
+    saveUninitialized: true,
+  })
+);
 
-app.use(auth)
-app.use('/shop', shopRoutes);
+// Enable CORS for all routes
+app.use(cors());
 
+// Database connection using an environment variable
+const mongodbUri = process.env.MONGODB_URI || "mongodb+srv://asmasrairi:56isr0D9O7p34eY0@cluster0.2bdnucf.mongodb.net/Ar_Got";
 
+mongoose.connect(mongodbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
-app.listen(13756, () => {
-    connect() ; 
-    console.log("listening on " + 13756);
-})
+app.use(auth);
+app.use("/shop", shopRoutes);
+
+// Dynamically determine the port or use a default value
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("Listening on port " + port);
+});
